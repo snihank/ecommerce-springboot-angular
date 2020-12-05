fdf9a5dc606d7c4068b56ee8357052a5c9fa9f71
Shakib Karim <shakib.karim@cognizant.com>
2020-12-05 15:14:26 -0500

    initial springboot app

---
+++ ecom_springboot/src/test/java/shopping_cart_app/com/shopping_cart_app/ShoppingCartAppApplicationTests.java
@@ -0,0 +1,13 @@
+package shopping_cart_app.com.shopping_cart_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
